a6cf60346f49dc1211c5a983f124b3a9f4f1d92c
marianoInsa <marianoinsaurralde5@gmail.com>
2024-05-08 02:06:11 -0300

    optimizacion del algorimo

--- src/Problema7.java
+++ src/Problema7.java
@@ -2,40 +2,32 @@
 
 // Euclid's sieve algorithm to find prime numbers
 
-import java.util.TreeMap;
-
 public class Problema7 {
-    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         int n = 10001;
-        TreeMap<Integer, Integer> primos = encontrarPrimos(n);
-        System.out.println("El 10001 numero primo es: " + primos.lastEntry().getValue());
+        int primo = encontrarPrimo(n);
+        System.out.println("El 10001 numero primo es: " + primo);
     }
 
-    @SuppressWarnings("rawtypes")
-    private static TreeMap encontrarPrimos(int n) {
-        TreeMap<Integer, Integer> primos = new TreeMap<>();
-        primos.put(0, 2);
-        int cont = 3;
+    private static int encontrarPrimo(int n) {
+        int primo = 0;
+        int cont = 2;
+        int i = 1;
 
-        for (int i = 1; i < n; i++) {
-            boolean encontro = true;
-            while (encontro) {    
-                boolean esPrimo = true;
-                for (int j = 2; j <= Math.sqrt(cont); j++) {
-                    if (cont % j == 0) {
-                        esPrimo = false;
-                        break;
-                    }
+        while (i <= n) {   
+            boolean esPrimo = true;
+            for (int j = 2; j <= Math.sqrt(cont); j++) {
+                if (cont % j == 0) {
+                    esPrimo = false;
+                    break;
                 }
-                if (esPrimo) {
-                    primos.put(i, cont);
-                    encontro = false;
-                }
-                cont++;
             }
-
+            if (esPrimo) {
+                primo = cont;
+                i++;
+            }
+            cont++;
         }
-        return primos;
+        return primo;
     }
 }
