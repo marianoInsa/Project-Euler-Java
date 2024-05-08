8813a5563b7866145b6c4b9877dc39ab14e31133
marianoInsa <marianoinsaurralde5@gmail.com>
2024-05-07 23:03:18 -0300

    optimizacion matematica: cada 3er numero de fibo es par

--- src/Problema2.java
+++ src/Problema2.java
@@ -1,21 +1,20 @@
 // Al considerar los términos en la secuencia de Fibonacci cuyos valores no superan los cuatro millones, encuentre la suma de los términos de valor par.
 
-public class Problema2 {
-    public static void main(String[] args) throws Exception {
-        
-        int array[] = {0, 1};
-        // array = new int[1];
-        int suma = 0;
-        int aux = 0;
-        while (aux < 4000000) {
-            aux = array[0] + array[1];
-            array[0] = array[1];
-            array[1] = aux;
+// Hay que aprovechar el patrón en la secuencia de Fibonacci: cada tercer número es par.
 
-            if (aux % 2 == 0) {
-                suma += aux;
-            }
-            System.out.println("Fibo: " + aux);
+public class Problema2 {
+    public static void main(String[] args) {
+        
+        int a = 1;
+        int b = 1;
+        int c = a + b;
+        int suma = 0;
+        
+        while (c < 4000000) {
+            suma += c;
+            a = b + c;
+            b = c + a;
+            c = a + b;
         }
         System.out.println("Suma de los terminos pares: " + suma);
     }
