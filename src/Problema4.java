16209d03a33ba7477769523741fb9f38a2d527b4
marianoInsa <marianoinsaurralde5@gmail.com>
2024-05-07 23:16:09 -0300

    optimizacion del problema

--- src/Problema4.java
+++ src/Problema4.java
@@ -1,26 +1,31 @@
 // Un número palindrómico lee lo mismo en ambos sentidos. El mayor palíndromo hecho del producto de dos números de 2 dígitos son 9009 = 91 x 99. 
 // Encuentra el palíndromo más grande hecho del producto de dos números de 3 dígitos.
 
+// Solución:
+// Reduzco el rango a 900
+// Recorrido en orden inverso para salir ante el primer palíndromo encontrado, que seguro sera el mayor
+// Uso de StringBuilder para invertir el producto y comparar con el original, ya que este metodo es mas eficiente que el uso de un ciclo for para invertir el numero caracter a caracter
+
 public class Problema4 {
 
     public static void main(String[] args) {
-        int numero;
-        String cadena = "";
-        String cadenaInvertida = "";
 
-        for (int i = 999; i >= 990; i--) {
+        int maximoPalindromo = 0;
+
+        for (int i = 999; i >= 900; i--) {
             for (int j = 999; j >= 900; j--) {
-                numero = i * j;
-                cadena = Integer.toString(numero);
-                cadenaInvertida = "";
-                for (int k = cadena.length() - 1; k >= 0; k--) {
-                    cadenaInvertida += cadena.charAt(k);
+                int producto = i * j;
+                if (producto <= maximoPalindromo) {
+                    break;
                 }
                 
-                if (cadena.equals(cadenaInvertida)) {
-                    System.out.println(cadena);
+                String productoString = Integer.toString(producto);
+                String reversoProducto = new StringBuilder(productoString).reverse().toString();
+                if (productoString.equals(reversoProducto)) {
+                    maximoPalindromo = producto;
                 }
             }
         }
+        System.out.println("El palindromo más grande es: " + maximoPalindromo);
     }
 }
