01f2d9c02ae2199bb42ed20af1ac90133a047ecc
marianoInsa <marianoinsaurralde5@gmail.com>
2024-05-07 22:54:13 -0300

    optimizacion matematica en el ejercicio

--- src/Problema1.java
+++ src/Problema1.java
@@ -1,16 +1,19 @@
 // Encontrar la suma de los multiplos de 3 o 5 abajo de 1000
 
+// Utilizando la fórmula de la suma de una serie aritmética para encontrar la suma de todos los múltiplos de un número dado en un rango
+
 public class Problema1 {
+
+    private static int sumaMultiplos(int n, int limite) {
+        int p = limite / n;
+        return n * (p * (p + 1)) / 2;
+    }
 
     public static void main(String[] args) {
         
-        int suma = 0;
-        for (int i = 1; i < 1000; i++) {
-            if ((i % 3 == 0) || (i % 5 == 0)) {
-                suma += i;
-            }
-        }
-        System.out.println(suma);
+        int limite = 1000 - 1;
+        int suma = sumaMultiplos(3, limite) + sumaMultiplos(5, limite) - sumaMultiplos(15, limite);
+        System.out.println("La suma de los múltiplos de 3 o 5 abajo de 1000 es: " + suma);
 
     }
 }
